c91228cd0f94c029a4bcfd4b43c35fcc2bef6d54
Ev-Vicky <vicky.yadav@evitamin.in>
2025-01-23 21:22:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def home():
     # Check if the user is already logged in
     if 'username' in session:
         return redirect(url_for('dashboard'))
-    return render_template('login.html')  # Displays the login form
+    return render_template('index.html')  # Displays the index form
 
 @app.route('/login', methods=['POST'])
 def login():
